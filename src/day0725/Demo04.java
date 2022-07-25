d4ce79e0d63368166b7ec6d6800041eb54117179
tarena <455374109@qq.com>
2022-07-25 14:11:02 +0800

    Change Tedu Start 07/21/2022
    Signed-off-by: jym <455374109@qq.com>
    Signed-off-by: tarena <455374109@qq.com>

---
+++ src/day0725/Demo04.java
@@ -0,0 +1,19 @@
+package day0725;
+
+public class Demo04 {
+    public static void main(String[] args) {
+        byte b = Byte.MAX_VALUE;
+        System.out.println(b);
+        short s = Short.MAX_VALUE;
+        System.out.println(s);
+        int n = Integer.MAX_VALUE;
+        System.out.println(n);
+        long l = Long.MAX_VALUE;
+        System.out.println(l);
+        float f = Float.MAX_VALUE;
+        System.out.println(f);
+        double d = Double.MAX_VALUE;
+        System.out.println(d);
+
+    }
+}
