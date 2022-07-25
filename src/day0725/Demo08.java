1d93ef377c99ab80ed3a7df294345aeb6aacbef1
tarena <455374109@qq.com>
2022-07-25 17:11:43 +0800

    Change Tedu Start 07/21/2022
    Signed-off-by: jym <455374109@qq.com>
    Signed-off-by: tarena <455374109@qq.com>

--- src/day0725/Demo08.java
+++ src/day0725/Demo08.java
@@ -3,16 +3,27 @@ package day0725;
 public class Demo08 {
     public static void main(String[] args) {
         int n = 5;
-        n++;
+        n++;//后加或者叫先取值,后增加
+        //先取n的值为n++整体表达式的值为5,后n增加1为6
         System.out.println(n);
         n = 5;
         n--;
         System.out.println(n);
         n = 5;
-        ++n;
+        ++n;//先增加,再取值
+        //先将n增加1再将增加1后的6赋值给n
         System.out.println(n);
         n = 5;
         --n;
         System.out.println(n);
+        //坑人题目,n1变化两次,一次变为9后变为8
+        int n1 = 8;
+        n1 = n1++;
+        System.out.println(n1);
+
+
+        int n2 = 8;
+        n2 = ++n2;
+        System.out.println(n2);
     }
 }
---
+++ src/day0725/Demo09.java
@@ -0,0 +1,9 @@
+package day0725;
+
+public class Demo09 {
+    public static void main(String[] args) {
+        int age = 22;
+        boolean isKid = age >= 12;
+        System.out.println(isKid);
+    }
+}
