f65e1c245f86c9283e2633170402d8bd3c367013
tarena <455374109@qq.com>
2022-07-25 17:37:31 +0800

    Change Tedu Start 07/21/2022
    Signed-off-by: jym <455374109@qq.com>
    Signed-off-by: tarena <455374109@qq.com>

---
+++ src/day0725/Demo10.java
@@ -0,0 +1,11 @@
+package day0725;
+
+public class Demo10 {
+    public static void main(String[] args) {
+
+        char gender = '女';
+        int age =666;
+        boolean pass = gender =='女' && age >= 60;
+        System.out.println(pass);
+    }
+}
