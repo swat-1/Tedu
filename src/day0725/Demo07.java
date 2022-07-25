501a69da3cf157ea97a3b802327b779e1eb88499
tarena <455374109@qq.com>
2022-07-25 16:32:50 +0800

    Change Tedu Start 07/21/2022
    Signed-off-by: jym <455374109@qq.com>
    Signed-off-by: tarena <455374109@qq.com>

--- src/day0725/Demo07.java
+++ src/day0725/Demo07.java
@@ -3,10 +3,32 @@ package day0725;
 public class Demo07 {
     public static void main(String[] args) {
 
+
         int n = 5;
         n = n + 5;
         System.out.println(n);
         n += 5;
         System.out.println(n);
+        System.out.println("==================");
+        n = 5;
+        n -= 5;
+        System.out.println(n);
+
+        n = 5;
+        n *= 5;
+        System.out.println(n);
+
+        n = 5;
+        n /= 5;
+        System.out.println(n);
+
+        n = 5;
+        n %= 5;
+        System.out.println(n);
+
+        int i = 5;
+        i *= 1.5;
+        System.out.println(i);
+
     }
 }
---
+++ src/day0725/Demo08.java
@@ -0,0 +1,18 @@
+package day0725;
+
+public class Demo08 {
+    public static void main(String[] args) {
+        int n = 5;
+        n++;
+        System.out.println(n);
+        n = 5;
+        n--;
+        System.out.println(n);
+        n = 5;
+        ++n;
+        System.out.println(n);
+        n = 5;
+        --n;
+        System.out.println(n);
+    }
+}
