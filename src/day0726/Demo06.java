d22f12a0f48b24e7c4116ad28e85b7f8750f600d
tarena <455374109@qq.com>
2022-07-26 16:29:40 +0800

    不同步out等文件
    Signed-off-by: tarena <455374109@qq.com>

---
+++ src/day0726/Demo06.java
@@ -0,0 +1,13 @@
+package day0726;
+
+public class Demo06 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = -8; i <= 2; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
