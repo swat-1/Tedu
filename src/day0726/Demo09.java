bb9e9fd6ee517b5bc907e2e719ceccf50101f023
tarena <455374109@qq.com>
2022-07-27 09:29:13 +0800

    练习了一会

--- src/day0726/Demo09.java
+++ src/day0726/Demo09.java
@@ -8,9 +8,15 @@ public class Demo09 {
         if(age == 8){
             int time =5;
             System.out.println(time);
+            //int times = 9; //同一个作用域中不能重复定义变量
         }
-    }if(age < 7){
-        int time =
+        //int age = 10; 同一个作用域中不能重复定义变量
+        if (age < 7){
+            int times = 10;
+            System.out.println(times);
+            System.out.println(age);
+        }
+        int k=9;
+        System.out.println(k);
     }
 }
-
