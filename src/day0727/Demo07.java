b8dcc553ab659604a1bbb8f4143c82116133428e
tarena <455374109@qq.com>
2022-07-27 17:12:41 +0800

    斗地主发牌

--- src/day0727/Demo07.java
+++ src/day0727/Demo07.java
@@ -1,22 +1,58 @@
 package day0727;
 
+import java.util.Random;
+
 public class Demo07 {
     public static void main(String[] args) {
-        String [] rank = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2" };
-        String [] cards = new String[54];
-        cards [cards.length-1] = "♀";
-        cards [cards.length-2] = "♂";
+        String[] rank = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
+        String[] cards = new String[54];
 
+        cards[cards.length - 1] = "♀";
+        cards[cards.length - 2] = "♂";
+        Random random = new Random();
         for (int i = 0; i < rank.length; i++) {
-            cards[i] ="♠"+rank[i];
-            cards[i+13] ="♥"+rank[i];
-            cards[i+13+13] ="♣"+rank[i];
-            cards[i+13+13+13] ="♦"+rank[i];
+            cards[i] = "♠" + rank[i];
+            cards[i + 13] = "♥" + rank[i];
+            cards[i + 13 + 13] = "♣" + rank[i];
+            cards[i + 13 + 13 + 13] = "♦" + rank[i];
         }
-
         for (int i = 0; i < cards.length; i++) {
-            System.out.print(cards[i] + " ");
+            int n = random.nextInt(cards.length);//0-54
+            String temp = cards[i];
+            cards[i] = cards[n];
+            cards[n] = temp;
+        }
+        for (int i = 0; i < cards.length; i++) {
+            System.out.print(cards[i]);
+        }
+        System.out.println();
+        String[] zhao = new String[17];
+        String[] qian = new String[17];
+        String[] sun = new String[17];
+
+        for (int i = 0; i < 17; i++) {
+            zhao[i] = cards[i * 3];
+            qian[i] = cards[i * 3 + 1];
+            sun[i] = cards[i * 3 + 2];
+
         }
 
+        for (int i = 0; i < sun.length; i++) {
+            System.out.print(sun[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < qian.length; i++) {
+            System.out.print(qian[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < zhao.length; i++) {
+            System.out.print(zhao[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i <3; i++) {
+            System.out.print(cards[53-i] + " ");
+        }
     }
- }
+
+}
---
+++ src/day0727/Demo08.java
@@ -0,0 +1,16 @@
+package day0727;
+
+public class Demo08 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+        int c = 0;
+
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
